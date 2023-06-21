147af3e15dcc758c0378186781fef65e93322a81
Paulo Henrique dos Santos <pauloengenharia5@yahoo.com>
2023-06-21 13:24:38 -0300

    Add files via upload

---
+++ desafio2.rb
@@ -0,0 +1,13 @@
+puts "Ola Amigo"
+puts "digite o seu nome completo"
+nome = gets.chomp
+
+puts "Seu nome é " + nome
+
+puts "Agora digite a sua idade"
+
+idade = gets.chomp 
+
+puts "Sua idade é" + idade
+
+puts "Olá #{nome}  Seja bem vindo! Sua idade é #{idade}." 
